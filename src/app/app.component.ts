f151501f90b39093982c70ea3b964175ff0319db
LironA.Cybrtx <LironA.Cybrtx@gmail.com>
2021-01-13 11:14:47 +0200

    New proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'customer-database',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  /**
+   * template: `
+            <customer-table></customer-table>            
+            `
+   */
 })
 export class AppComponent {
   title = 'CustomerRecords';
